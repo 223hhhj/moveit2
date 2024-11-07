4b98c72ce377416435a1c3ec52863262d5bd925e
223hhhj <mike355116@gmail.com>
2024-11-07 15:08:23 +0800

    1107-1

---
+++ workspace/build/ur_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/steven/workspace/src/Universal_Robots_ROS2_Driver/ur_bringup
+# Build directory: /home/steven/workspace/build/ur_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ workspace/build/ur_controllers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/steven/workspace/src/Universal_Robots_ROS2_Driver/ur_controllers
+# Build directory: /home/steven/workspace/build/ur_controllers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
